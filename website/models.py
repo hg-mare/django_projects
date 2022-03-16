76dfaa26c47c650644dd422ecebb3b1f9c272c5d
hgmr <hg.mare.cute@gmail.com>
2022-03-16 21:26:33 +0900

    add

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (("revo","リボ"), ("installment_payment","分割払"))
+class Debt(models.Model):
+    lender = models.CharField(max_length=200)
+    total_amount = models.IntegerField(default=0)
+    interest_rate = models.FloatField()
+    manthly_repayment = models.IntegerField(default=0)
+    repayment_method = models.CharField(max_length=50, choices=CATEGORY)
+
+    def __str__(self):
+        return self.lender
